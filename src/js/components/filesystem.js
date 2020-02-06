8512b7a087dd1b5b100f6a1c3efa30264239d8c4
lpscrypt <lpscrypt@gmail.com>
2020-02-06 18:02:18 -0500

    styling

--- src/js/components/filesystem.js
+++ src/js/components/filesystem.js
@@ -15,20 +15,22 @@ export class FileSystem extends Component {
   }
 
     render() {
-      console.log('fs props', this.props)
+      // console.log('fs props', this.props)
       const deskKeys = this.props.pathname
                    .split('/')
                    .slice(2);
-      console.log('dk',deskKeys);
+      // console.log('dk',deskKeys);
       const loc = deskKeys[0] === '' ?  'dir' :
                     'dir.'.concat(deskKeys
                    .join('.dir.').concat('.dir'));
-      console.log('fs loc', loc)
+      // console.log('fs loc', loc)
       const dirObj = _.get(this.state.hon, loc, false);
-      console.log('fs desk', this.state.hon)
-      console.log('fs dirObj', this.dirObj)
+      // console.log('fs desk', this.state.hon)
+      // console.log('fs dirObj', this.dirObj)
       return _.keys(dirObj).map((key, i) => (
+          <div className="card ba br3 grow bg-navy washed-green fl w-20">
           <p key={i} className="lh-copy measure pt3">Directory: {key}</p>
+          </div>
       ));
     }
 }
